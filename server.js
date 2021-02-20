cb570f766cdc80b899159db7c61929862c3688a9
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-02-20 10:53:24 +0100

    set up CRUD routes bootcamp without express router

--- server.js
+++ server.js
@@ -6,6 +6,39 @@ dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: 'Show all bootcamps - still wait for db' })
+})
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Show bootcamp ${req.params.id} - still wait for db`,
+  })
+})
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: 'Create new Bootcamp - still wait for db' })
+})
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update bootcamp ${req.params.id} - still wait for db`,
+  })
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootcamp ${req.params.id} - still wait for db`,
+  })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(
