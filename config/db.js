b72aeaa5d9a8313543490784503869a3433aa3d9
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-02-22 14:58:18 +0100

    init and connect mongoose

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`)
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-
-// route files
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
 
+// Connect to database
+connectDB()
+
+// route files
+const bootcamps = require('./routes/bootcamps')
+
 const app = express()
+
 // Dev loggin middleware
 process.env.NODE_ENV === 'development' && app.use(morgan('dev'))
 
@@ -17,7 +22,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
+
+// Handle undhandled promise rejections - dont use try catch at db.js
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  // close server & exit process
+  server.close(() => process.exit(1))
+})
