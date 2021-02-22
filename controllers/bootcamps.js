b74f535df4f071447694909975e750d879a0ac8d
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-02-22 16:14:18 +0100

    get bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,22 +3,36 @@ const Bootcamp = require('../models/Bootcamp')
 // @desc     Get all bootcamps
 // @route    GET /api/v1/bootcamps
 // @access   Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: 'Show all bootcamps - still wait for db',
-    hello: req.hello,
-  })
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find({})
+    res.status(200).json({
+      success: true,
+      data: bootcamps,
+    })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc     Get single bootcamp
 // @route    GET /api/v1/bootcamps/:id
 // @access   Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: `Show bootcamp ${req.params.id} - still wait for db`,
-  })
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id)
+
+    if (!bootcamp) {
+      res.status(400).json({ success: false })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    })
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc     Create new bootcamp
