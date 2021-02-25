ebe449c6e823797b2c07a37e7264872900e3d7fc
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-02-25 12:16:11 +0100

    impl geocoder and save formattedAddress

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
+const geocoder = require('../utils/geocoder')
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -103,6 +104,26 @@ const BootcampSchema = new mongoose.Schema({
 BootcampSchema.pre('save', function (next) {
   // console.log('slugify ran', this.name)
   this.slug = slugify(this.name, { lower: true })
+  next()
+})
+
+// geocode & create location field
+// doc: https://nchaulet.github.io/node-geocoder/
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address)
+  console.log('loc GEOCODER', loc)
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  }
+  // Do not save address in DB -> we are using formattedAddress
+  this.address = undefined
   next()
 })
 
