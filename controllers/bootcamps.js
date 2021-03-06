4d3c160e586eb5d0762b2e59003cdf4e92799d77
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-03-06 11:58:56 +0100

    add sort and select to query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,16 +8,42 @@ const Bootcamp = require('../models/Bootcamp')
 // @access   Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query
-  let queryStr = JSON.stringify(req.query)
 
+  const reqQuery = { ...req.query }
+  console.log(req.query)
+  // fields to exclude
+  const removeFields = ['select', 'sort']
+
+  // loop over removeFields and delete them from reqQuery
+  removeFields.forEach(param => delete reqQuery[param])
+
+  let queryStr = JSON.stringify(reqQuery)
+  console.log('queryStr', queryStr)
   // averageCost[gte]=10000 or careers[in]=Business
-  // careers: [ "Web Development", "UI/UX", "Business"]
   // https://mongoosejs.com/docs/tutorials/query_casting.html
+  // create operators ($gt, $gte, etc)
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
   // query string = /api/v1/bootcamps?location.city=Lowell&careers[in]=UI/UX
   // ****queryString {"location.city":"Lowell","careers":{"$in":"UI/UX"}}
+  // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr))
+
+  // Select Fields - docs: https://mongoosejs.com/docs/queries.html
+  if (req.query.select) {
+    // turn value of query params select in an array delimiter ","
+    // and transform back to string with space due to mongoose docs
+    const fields = req.query.select.split(',').join(' ')
+
+    query = query.select(fields)
+  }
+  // sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ')
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt')
+  }
 
   const bootcamps = await query
 
