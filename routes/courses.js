155a5f182f9fafa8eadd83382f38c3dd1cc60100
Sven Kusebauch <sven.kusebauch@gmail.com>
2021-03-16 15:17:45 +0100

    update and delete course

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,11 @@
 const express = require('express')
-const { getCourses, getCourse, addCourse } = require('../controllers/courses')
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../controllers/courses')
 
 // merge params from bootcamp
 const router = express.Router({ mergeParams: true })
@@ -7,6 +13,6 @@ const router = express.Router({ mergeParams: true })
 // prefix is always /:bootcampId/courses (defined in bootcamp routes)
 router.route('/').get(getCourses).post(addCourse)
 
-router.route('/:id').get(getCourse)
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse)
 
 module.exports = router
